da484243699a501366f512faba31dab268ec4e77
Isuru414 <50751910+Isuru414@users.noreply.github.com>
2019-05-25 23:13:42 +0530

    Add files via upload

---
+++ payment/sellnbuy.sql
@@ -0,0 +1,48 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 25, 2019 at 07:41 PM
+-- Server version: 10.1.35-MariaDB
+-- PHP Version: 7.2.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pay`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sellnbuy`
+--
+
+CREATE TABLE `sellnbuy` (
+  `id` int(10) NOT NULL,
+  `date` varchar(12) NOT NULL,
+  `details` varchar(30) NOT NULL,
+  `total` int(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `sellnbuy`
+--
+
+INSERT INTO `sellnbuy` (`id`, `date`, `details`, `total`) VALUES
+(100, '10012019', 'card', 24000);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
